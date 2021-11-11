7fee47b40cbc8a8d41a38abb843c9b0a9971aefc
GuiCoelho-S <gs.coelho@outlook.com>
2021-11-11 10:01:44 -0300

    criação do header

--- pokeapi-desafio/src/components/Header/index.jsx
+++ pokeapi-desafio/src/components/Header/index.jsx
@@ -1,8 +1,13 @@
 import react from 'react';
 import * as S from './style'
+import Pokebola from '../../assets/images/pokebola.png'
 export default function Header() {
 
     return(
-        <h1>Hello world</h1>
+        <S.ContainerHeader>
+            <span></span>
+            <img src={Pokebola} alt="logo do pokemon" />
+            <span></span>
+        </S.ContainerHeader>
     )
 }
--- pokeapi-desafio/src/components/Header/style.js
+++ pokeapi-desafio/src/components/Header/style.js
@@ -5,4 +5,23 @@ export const ContainerHeader = styled.div`
 width:100%;
 height:60px;
 border:1px solid red;
+display:flex;
+align-items:center;
+
+span {
+    width:50%;
+    height:60px;
+}
+
+span:nth-child(1){
+    background-color:red;
+}
+span:nth-child(2){
+    background-color:blue;
+}
+
+img {
+    width:50px;
+    height:50px;
+}
 `
