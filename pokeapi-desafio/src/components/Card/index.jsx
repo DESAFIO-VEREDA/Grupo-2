c2eeee66df040e38ca455da748314c552191ad32
GuiCoelho-S <gs.coelho@outlook.com>
2021-11-11 10:42:39 -0300

    criação do card

---
+++ pokeapi-desafio/src/components/Card/index.jsx
@@ -0,0 +1,12 @@
+import * as S from './style'
+
+export default function CardPokemon(){
+
+    return(
+        <S.CardPokemonContainer>
+            <S.InfoPokemon>
+                <h2>C</h2>
+            </S.InfoPokemon>
+        </S.CardPokemonContainer>
+    )
+}
---
+++ pokeapi-desafio/src/components/Card/style.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const CardPokemonContainer = styled.article`
+
+width:207px;
+height:207px;
+background-color:${(props) => props.color};
+display:flex;
+align-items: center;
+
+`
+
+export const InfoPokemon = styled.aside`
+
+align-self:flex-start;
+width:120px;
+height:100%;
+display:flex;
+flex-direction: column;
+justify-content: space-evenly;
+`
