7ddc3b14dd0ad4ff5925a4228441c88673281460
GuiCoelho-S <gs.coelho@outlook.com>
2021-11-11 11:58:06 -0300

    fetch

--- pokeapi-desafio/src/components/Card/index.jsx
+++ pokeapi-desafio/src/components/Card/index.jsx
@@ -1,12 +1,30 @@
+import { useEffect, useState } from 'react'
 import * as S from './style'
 
-export default function CardPokemon(name, type01, type02){
+export default function CardPokemon({name, url}){
+
+    const [dataPokemon, setDataPokemon ] = useState('');
+    
+    useEffect(() => {
+        const urlPokemon = url
+        console.log(urlPokemon)
+        async function fetchData() {
+          const request = await fetch(urlPokemon);
+          const results = await request.json();
+    
+          setDataPokemon(results);
+        }
+        fetchData();
+        
+      }, []);
 
     return(
         <S.CardPokemonContainer>
             <S.InfoPokemon>
-                <h2>Pikachu</h2>
-                
+                <h2>{name}</h2>
+                <div>
+                {dataPokemon.types.map((item) => <p>{item.type.name}</p>)}
+                </div>
                 <p>tipo 01</p>
                 <p>tipo 02</p>
             </S.InfoPokemon>
--- pokeapi-desafio/src/pages/home.jsx
+++ pokeapi-desafio/src/pages/home.jsx
@@ -5,23 +5,24 @@ import { ContainerCard } from '../components/ContainerCard';
 export default function Home() {
     const [pokemonList, setPokemonList] = useState([]);
     useEffect(() => {
-        const API = 'https://pokeapi.co/api/v2/pokemon/?offset=151&limit=302';
+        const API = 'https://pokeapi.co/api/v2/pokemon/?offset=151&limit=161';
         async function fetchData() {
           const request = await fetch(API);
           const json = await request.json();
           const results = json.results;
-          console.log(results)
           setPokemonList(results);
         }
         fetchData();
       }, []);
 
+
     return(
         <>
         <div>Main</div>
         <ContainerCard>
-            <CardPokemon />
-            <CardPokemon />
+            {
+                pokemonList.map((item) => <CardPokemon url={item.url} name={item.name} />)
+            }
         </ContainerCard>
         </>
     )
