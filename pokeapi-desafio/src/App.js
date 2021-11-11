4b1452729d1ab71ca4b4feb007019279dcc59add
GuiCoelho-S <gs.coelho@outlook.com>
2021-11-11 10:51:00 -0300

    aaaa

--- pokeapi-desafio/src/App.js
+++ pokeapi-desafio/src/App.js
@@ -1,19 +1,11 @@
-<<<<<<< HEAD
-import React from 'react';
-import { Router } from './components';
-
-function App() {
-  return (
-    <div>
-      <Router />
-=======
 import Header from "./components/Header";
+import { Router } from './components/'
 
 function App() {
   return (
     <div className="App">
       <Header />
->>>>>>> 4d64ec79027c7167e54404d95e5a87247c2ae904
+      <Router />
     </div>
   );
 }
