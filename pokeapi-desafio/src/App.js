e74cff8caaa82f95d491bee98c5321249d53a1b0
Vinicius Santos <vinicius.3991@gmail.com>
2021-11-11 11:17:11 -0300

    add fetch pokemon

--- pokeapi-desafio/src/App.js
+++ pokeapi-desafio/src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import { Router } from './components';
 import Header from './components/Header';
@@ -6,14 +5,6 @@ import Header from './components/Header';
 function App() {
   return (
     <div>
-=======
-import Header from "./components/Header";
-import { Router } from './components/'
-
-function App() {
-  return (
-    <div className="App">
->>>>>>> 4b1452729d1ab71ca4b4feb007019279dcc59add
       <Header />
       <Router />
     </div>
--- pokeapi-desafio/src/pages/home.jsx
+++ pokeapi-desafio/src/pages/home.jsx
@@ -1,6 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 export default function Home() {
+    const [pokemonList, setPokemonList] = useState([]);
+    useEffect(() => {
+        const API = 'https://pokeapi.co/api/v2/pokemon/?offset=151&limit=302';
+        async function fetchData() {
+          const request = await fetch(API);
+          const json = await request.json();
+          const results = json.results;
+          console.log(results)
+          setPokemonList(results);
+        }
+        fetchData();
+      }, []);
+
     return(
         <div>Main</div>
     )
